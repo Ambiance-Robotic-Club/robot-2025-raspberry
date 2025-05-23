71d0bfa6ed56a06b6d126ef468ce573fb8211286
Halyre <damien.pirola@gmail.com>
2025-05-23 21:57:52 +0200

    fix

--- src/raspberry/strategy/strategy.py
+++ src/raspberry/strategy/strategy.py
@@ -48,7 +48,7 @@ class Strategy:
             self.robot_busy = self.robot.get_busy(10) if not(self.init) else False
 
             #### Path correction ####
-            if self.actual_type_consigne == 1:
+            if self.actual_type_consigne == 2:
                 line = math.sqrt((self.actual_x - self.line_start_x) ** 2 + (self.actual_y - self.line_start_y) ** 2)
                 self.error_line = abs(math.sin(self.theoric_theta - self.actual_theta)*line)
             else:
