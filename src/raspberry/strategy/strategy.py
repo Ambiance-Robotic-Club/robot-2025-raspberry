ba50a96e22429cc773a055177e5f39e3c5386edd
Halyre <damien.pirola@gmail.com>
2025-05-25 21:37:52 +0200

    Fix

--- src/raspberry/strategy/strategy.py
+++ src/raspberry/strategy/strategy.py
@@ -89,12 +89,12 @@ class Strategy:
                 self.actual_type_consigne = (self.actual_type_consigne + 1) % 3
                 self.is_consigne = False
         
-            if abs(self.error_line) > constant.DISTANCE_ERROR_CORRECTION:
-                self.consigne_queue.insert(0,self.step_consigne)                
-                self.robot.send_position_consigne(50)
-                self.actual_type_consigne = 0
-                self.update_robot()
-                print("------- Recalcul consigne -------")
+            # if abs(self.error_line) > constant.DISTANCE_ERROR_CORRECTION:
+            #     self.consigne_queue.insert(0,self.step_consigne)                
+            #     self.robot.send_position_consigne(50)
+            #     self.actual_type_consigne = 0
+            #     self.update_robot()
+            #     print("------- Recalcul consigne -------")
     
     def process_step(self):
         if self.actual_type_consigne == 0:
