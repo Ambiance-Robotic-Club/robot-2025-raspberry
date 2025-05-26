8f594552e30d41bd3d0aac0d6645b678451bf2df
Halyre <damien.pirola@gmail.com>
2025-05-26 10:59:24 +0200

    Add sts3215 consigns

--- src/raspberry/strategy/map.py
+++ src/raspberry/strategy/map.py
@@ -11,8 +11,8 @@ class Map:
 
         self.objects = [pos_objects[i] for i in [0, 1, 2, 3, 4, 6, 7, 8, 9]] if self.color == "Yellow" else [pos_zone[i] for i in [1, 2, 3, 4, 5, 6, 7, 8, 9]]
 
-        self.objects = [[1225,1400,90]]
-        self.zone_end = pos_zone[0] if self.color == "Yellow" else pos_zone[3]
+        self.objects = [[825,275,-90]]
+        self.zone_end = pos_zone[3] if self.color == "Yellow" else pos_zone[0]
     #TO DO
     def moove_object_to_areas(self, area, object):
         pass
