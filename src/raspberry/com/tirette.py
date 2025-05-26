2aa35e57c04f42e29462151745a626b82fd4e9fd
Halyre <damien.pirola@gmail.com>
2025-05-26 17:15:11 +0200

    Add can consigns

--- src/raspberry/com/tirette.py
+++ src/raspberry/com/tirette.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as GPIO
 import time
-
+import utils.constant as constant
 servo_positions_init = [
     [10, 120, 55, 180, 155, 0, 0, 0, 0, 0,  0, 30, 0, 140, 55, 140],  
     [30, 120, 55, 90, 155, 0, 0, 0, 0, 0, 0, 30, 90, 140, 55, 125]]
@@ -19,6 +19,18 @@ def wait_tirette(pin, screen, servos, sts3215):
             for servo_id in range(16):
                 servos[servo_id].angle = servo_positions_init[1][servo_id]
 
+            time.sleep(1)
+            for step in constant.SERVOS_GET_CAN:
+                for servo_id in range(16):
+                    servos[servo_id].angle = step[servo_id]
+                time.sleep(0.5)
+
+            time.sleep(3)
+            for step in constant.DEPOSE_CAN:
+                for servo_id in range(16):
+                    servos[servo_id].angle = step[servo_id]
+                time.sleep(0.5)  
+        
             sts3215[0].homing()
             sts3215[1].homing()
 
