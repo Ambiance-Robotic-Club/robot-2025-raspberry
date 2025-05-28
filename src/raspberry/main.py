b6b13c65887606261313581e1c76c047c9966db4
Halyre <damien.pirola@gmail.com>
2025-05-28 18:13:45 +0200

    Verify deplacment

--- src/raspberry/main.py
+++ src/raspberry/main.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
     map = Map(color)
     robot.init_zone_start(zone_start)
 
-    robot.init_zone_start(10)
+    robot.init_zone_start(11)
     timerStart = time.time()
     timer = 0
     
