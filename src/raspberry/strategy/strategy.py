25be299fa34ce4157f95c180b41ba37d8c4cf0f2
Halyre <damien.pirola@gmail.com>
2025-05-28 11:13:54 +0200

    Add pami send color

--- src/raspberry/strategy/strategy.py
+++ src/raspberry/strategy/strategy.py
@@ -271,6 +271,7 @@ def banniere(robot, sts3215, servos):
     sts3215[0].set_position_calib(11000)
     sts3215[1].set_position_calib(11000)
 
+    time.sleep(0.5)
     while sts3215[0].is_busy() or sts3215[1].is_busy():
         pass
     
