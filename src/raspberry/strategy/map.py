10133bf574bc416db8adbb96b2c9ad570c73e225
Halyre <damien.pirola@gmail.com>
2025-05-29 02:18:13 +0200

    Add message screen

--- src/raspberry/strategy/map.py
+++ src/raspberry/strategy/map.py
@@ -12,9 +12,7 @@ class Map:
 
         self.objects = [pos_objects[i] for i in [2]] if self.color == "Yellow" else [pos_zone[i] for i in [7]]
         #Yellow : [0, 1, 2, 3, 4, 6, 7, 8, 9] Blue : [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        self.objects = [pos_objects[7]]
         self.objects_a_pousser = [] #pos_objects[9]
-        self.our_zones = [pos_zone[5]]
         self.zone_end = pos_zone[3] if self.color == "Yellow" else pos_zone[0]
     #TO DO
     def moove_object_to_areas(self, area, object):
